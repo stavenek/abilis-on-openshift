39f971de1e2579c60105c3a771e150ddb1a0a9f4
joachim.bjorklund <joachim.bjorklund@netent.com>
2015-04-12 22:10:14 +0200

    commited

--- new-axel.sh
+++ new-axel.sh
@@ -9,6 +9,7 @@ if [ -d "$1" ]; then
     echo "app already exists"
     exit
 fi
+rhc_app=$1
     
 
 if [ -z "$2" ]; then
@@ -16,13 +17,14 @@ if [ -z "$2" ]; then
     exit
 fi
 
-rhc app create $1 diy-0.1 mysql-5.5 -l $2 || exit
-cd $1
+rhc app create $rhc_app diy-0.1 mysql-5.5 -l $2 || exit
+cd $rhc_app
 git remote add knowly-axel-openshift git://github.com/joachimbjorklund/knowly-axel-openshift.git
 git pull --no-edit -s recursive -X theirs knowly-axel-openshift master 
-rhc app show $1 | tee rhc.txt
+rhc app show $rhc_app | tee rhc.txt
 git rm -r misc
 git rm diy/index.html
 git rm diy/testrubyserver.rb
 git commit -m "removed"
 git push
+
